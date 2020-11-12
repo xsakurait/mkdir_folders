0008a63c14daa29d580a9f8239589e02de100be6
xsakurait <60243830+xsakurait@users.noreply.github.com>
2020-11-12 13:18:37 +0900

    Add files via upload

---
+++ フォルダを作る.py
@@ -0,0 +1,11 @@
+#これを実行することで、フォルダにexcelに書いたフォルダ名を持つ複数のフォルダを作ることができる
+import os
+import csv
+stores_path = "店舗コード.csv"
+
+with open(stores_path, encoding="cp932") as f:
+    reader= csv.reader(f)
+    for row in  reader:
+        folder_name  = row[0] + '_' + row[1]
+        if not os.path.exists( folder_name):
+            os.mkdir( folder_name)
